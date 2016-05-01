6c3f1275b418bb5c03ff462aa80789a16c8b8ce9
Enrique Toomey <enritoomey@gmail.com>
2016-05-01 00:47:01 -0300

    Formatting GUI layout

--- layout_CiclosApp.py
+++ layout_CiclosApp.py
@@ -2,13 +2,12 @@
 
 # Form implementation generated from reading ui file 'layout_CiclosApp.ui'
 #
-# Created: Thu Jan 14 00:07:19 2016
+# Created: Sun May  1 00:09:57 2016
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
-
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
@@ -21,14 +20,12 @@ class Ui_Dialog(object):
         sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
         Dialog.setSizePolicy(sizePolicy)
         Dialog.setSizeGripEnabled(True)
-        self.verticalLayoutWidget = QtGui.QWidget(Dialog)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 241, 191))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
+        self.gridLayout = QtGui.QGridLayout(Dialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetMinAndMaxSize)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.label = QtGui.QLabel(self.verticalLayoutWidget)
+        self.label = QtGui.QLabel(Dialog)
         palette = QtGui.QPalette()
         brush = QtGui.QBrush(QtGui.QColor(255, 0, 0))
         brush.setStyle(QtCore.Qt.SolidPattern)
@@ -80,15 +77,16 @@ class Ui_Dialog(object):
         self.label.setWordWrap(False)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
-        self.textBrowser = QtGui.QTextBrowser(self.verticalLayoutWidget)
+        self.textBrowser = QtGui.QTextBrowser(Dialog)
         self.textBrowser.setObjectName("textBrowser")
         self.verticalLayout.addWidget(self.textBrowser)
-        self.SabatheButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.SabatheButton = QtGui.QPushButton(Dialog)
         self.SabatheButton.setObjectName("SabatheButton")
         self.verticalLayout.addWidget(self.SabatheButton)
-        self.JouleBraytonButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.JouleBraytonButton = QtGui.QPushButton(Dialog)
         self.JouleBraytonButton.setObjectName("JouleBraytonButton")
         self.verticalLayout.addWidget(self.JouleBraytonButton)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -97,14 +95,11 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QtGui.QApplication.translate("Dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
         self.label.setText(QtGui.QApplication.translate("Dialog", "Ciclos App", None, QtGui.QApplication.UnicodeUTF8))
-        self.textBrowser.setHtml(QtGui.QApplication.translate("Dialog",
-                                                              "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-                                                              "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-                                                              "p, li { white-space: pre-wrap; }\n"
-                                                              "</style></head><body style=\" font-family:\'Noto Sans\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-                                                              "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-style:italic;\">Esta App permite calcular los ciclos termodinámicos de Joule-Brayton, Joule-Brayton con post-combustion y Sabathe.</span></p></body></html>",
-                                                              None, QtGui.QApplication.UnicodeUTF8))
-        self.SabatheButton.setText(
-            QtGui.QApplication.translate("Dialog", "Ciclo Sabathe", None, QtGui.QApplication.UnicodeUTF8))
-        self.JouleBraytonButton.setText(
-            QtGui.QApplication.translate("Dialog", "Ciclo Joule-Brayton", None, QtGui.QApplication.UnicodeUTF8))
+        self.textBrowser.setHtml(QtGui.QApplication.translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Noto Sans\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-style:italic;\">Esta App permite calcular los ciclos termodinámicos de Joule-Brayton, Joule-Brayton con post-combustion y Sabathe.</span></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.SabatheButton.setText(QtGui.QApplication.translate("Dialog", "Ciclo Sabathe", None, QtGui.QApplication.UnicodeUTF8))
+        self.JouleBraytonButton.setText(QtGui.QApplication.translate("Dialog", "Ciclo Joule-Brayton", None, QtGui.QApplication.UnicodeUTF8))
+
