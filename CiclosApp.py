1b2f89e8fad91864b51dff993d6779927c1dfb63
enri <enritoomey@gmail.com>
2016-06-12 15:27:05 -0300

    adding subfolders to sys.path

--- CiclosApp.py
+++ CiclosApp.py
@@ -1,11 +1,19 @@
 import sys
+import imp
 
 from PySide.QtCore import *
 from PySide.QtGui import *  # importo todas las funciones de pyside
 
+# TODO: improve the importation of sub-folders
+sys.path.append('./JOULE_BRAYTON')
+sys.path.append('./SABATHE')
+sys.path.append('./common')
+
+
 import layout_CiclosApp
 from Ciclo_JouleBrayton_App import JouleBraytonDialog
 from Ciclo_Sabathe_App import SabatheDialog
+
 
 __appName__ = 'Ciclos App'
 
